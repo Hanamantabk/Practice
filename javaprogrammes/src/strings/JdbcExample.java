a46f196823b473a12b93669fe919a84450d0f141
user <user@DESKTOP-ECH0NJE>
2022-01-06 23:40:48 +0530

    complete jdbc steps

--- javaprogrammes/src/strings/JdbcExample.java
+++ javaprogrammes/src/strings/JdbcExample.java
@@ -5,38 +5,36 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
 public class JdbcExample {
 
 	public static void main(String[] args) {
-		String db="mydb";
-		String username="root";
-		String password="root";
-		String sql="select * from sports";
-		String url=("jdbc:mysql://localhost:3306/"+db);
-		//register
-		
-		try {
-		//Class.forName("com.mysql.jdbc.Driver");
-		Connection con=DriverManager.getConnection(url, username, password);
-		Statement stmt=con.createStatement();
-		ResultSet rst =stmt.executeQuery(sql);
-		
-		while(rst.next()) {
-			System.out.println("results are id"+rst.getInt(1)+" name "+rst.getString(2));
-		}
-		
-		}
-		catch(Exception e)
-		{
-			System.out.println("exception is "+e);
-		}
-		
-		//connection
-		
-		//create statement
+		String db = "mydb";
+		String username = "root";
+		String password = "root";
+		String sql = "select * from sports";
+		String url = ("jdbc:mysql://localhost:3306/" + db);
 
-		//result
+		try {
+			// register-is depricated step
+			// Class.forName("com.mysql.jdbc.Driver");
+
+			// establishing connection
+			Connection con = DriverManager.getConnection(url, username, password);
+
+			// creating statement obj
+			Statement stmt = con.createStatement();
+
+			// get resultSet by passing query
+			ResultSet rst = stmt.executeQuery(sql);
+
+			while (rst.next()) {
+				System.out.println("results are id" + rst.getInt(1) + " name " + rst.getString(2));
+			}
+
+		} catch (Exception e) {
+			System.out.println("exception is " + e);
+		}
+
 	}
 
 }
